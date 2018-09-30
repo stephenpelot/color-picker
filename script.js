92ff7febac233905014aebdadb195d3aa320c3e4
stephenpelot <43397850+stephenpelot@users.noreply.github.com>
2018-09-30 12:31:53 -0600

    Add files via upload
    added index.html, script.js, and style.css

---
+++ script.js
@@ -0,0 +1,35 @@
+window.onload = function()  {
+	initColorPicker ();
+};
+
+function initColorPicker() {
+	let colorBox = document.getElementById("color-box")
+	let rgb = {
+		red: document.getElementById("red"),
+		green: document.getElementById("green"),
+		blue: document.getElementById("blue"),
+	};
+
+}
+
+function setColorPickerEventListeners (colorBox, rgb) {
+	rgb.red.addEventListener("change" , () =>{
+		console.log("Red value: ", rgb.red.value);
+		setBoxGBColor(colorBox, 150, 150, 150);
+	})
+	{
+		rgb.green.addEventListener("change" , () =>{
+			console.log("Green value: ", rgb.green.value);
+		})
+	} {
+		rgb.blue.addEventListener("change" , () =>{
+			console.log("Blue value: ", rgb.blue.value);
+		})
+	}
+}
+
+
+function setBoxBGColorf(ColorBox , red, green, blue) {
+	rgbVal = [red, green, blue].join(',');
+	colorBox.style.backgroundColor = "rgb(" + rgbVal + ")";
+}
